b878c33937d53ced19ebde07114e55e825c61e5b
Ksualboy <danielmillanalvarez@gmail.com>
2021-08-15 20:08:47 +0000

    making a mistake

--- 1-print_square.js
+++ 1-print_square.js
@@ -15,7 +15,7 @@ if (process.argv.length <= 2) {
 }
 
 size = parseInt(process.argv[2], 16)
-print(size)
+process.stdout.write(size)
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
